ff6e1bf1c58cf0162f27686f258edfe2faf0c0a2
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-12 14:00:26 +0100

    added about & meta data files

---
+++ app/data/meta.js
@@ -0,0 +1,5 @@
+export const metaData = {
+  siteAuthor: "Eszter O'Donoghue",
+  siteTitle: "Hello New Life",
+  siteDescription: "Thoughts and writtings on my new life",
+};
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
+import { about } from "./data/about.js";
+import { metaData } from "./data/meta.js";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Hello New Life | Eszter O'Donoghue",
-  description: "Thoughts and writtings on my new life",
+  title: `${metaData.siteTitle} | ${metaData.siteAuthor}`,
+  description: `${metaData.siteDescription}`,
 };
 
 export default function RootLayout({ children }) {

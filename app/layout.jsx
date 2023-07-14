c3c05f007823c07bd35f0b05177de56da4118d22
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-14 11:26:19 +0100

    uninstalled graphql graphql-request html-react-parser moment & switched off vercel analytics for now

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/react";
+// import { Analytics } from "@vercel/analytics/react";
 import Header from "./components/Header";
 import { about } from "./data/about.js";
 import { metaData } from "./data/meta.js";
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className} container mx-auto px-4 `}>
         <Header />
         {children}
-        <Analytics />
+        {/* <Analytics /> */}
       </body>
     </html>
   );

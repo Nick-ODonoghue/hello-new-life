4dbbfac4e5b9b239d2ee092030c4aec72f028a88
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-17 15:31:07 +0100

    added dark theme switcher helped with next-themes

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import { header } from "../data/header.js";
+import ThemeButton from "./ThemeButton.jsx";
 
 export default function Header() {
   return (
     <header className=" py-6">
-      <h1 className=" text-2xl">{header.title}</h1>
+      <nav className=" flex justify-between items-center">
+        <Link href="/">
+          <h1 className=" text-2xl">{header.title}</h1>
+        </Link>
+        <ThemeButton />
+      </nav>
     </header>
   );
 }

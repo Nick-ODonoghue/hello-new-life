4123708b6637ba34c08c41f997564e62ce891edc
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-18 16:36:26 +0100

    ERROR FIX: hardcoding projectId, dataset & apiVersion

--- app/lib/sanity.js
+++ app/lib/sanity.js
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+const projectId = "5l6gydcx";
+const dataset = "production";
+const apiVersion = "2023-07-18";
 
 export const client = createClient({
   projectId,

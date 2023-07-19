342ea3c887d42c93d60adc6509b115edce2ea0af
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-19 10:18:03 +0100

    ADDED: sanity/imageUrl & implemented image rendering from the sanity rich text component

--- app/post/[slug]/page.jsx
+++ app/post/[slug]/page.jsx
@@ -1,4 +1,7 @@
+import { PortableText } from "@portabletext/react";
 import { client } from "../../lib/sanity";
+import Image from "next/image";
+import { urlFor } from "@/app/lib/sanityImageUrl";
 
 async function getPost(slug) {
   const query = `*[_type == "post" && slug.current == "${slug}"][0]`;
@@ -8,10 +11,22 @@ async function getPost(slug) {
 
 export default async function page({ params }) {
   const post = await getPost(params.slug);
+
+  const myPortableTextComponent = {
+    types: {
+      image: ({ value }) => {
+        return <Image src={urlFor(value.asset._ref).url()} alt={value.alt} width={540} height={360} />;
+      },
+    },
+  };
+
   return (
     <>
-      <div className=" space-y-2">
+      <div className=" mb-6">
         <h1 className=" text-2xl max-w-fit border-b-2 border-pink-500">{post.title}</h1>
+      </div>
+      <div className=" space-y-6">
+        <PortableText value={post.content} components={myPortableTextComponent} />
       </div>
     </>
   );

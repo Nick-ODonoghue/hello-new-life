5d6d3b7e2f931a6621fe8392d147aec2281738ae
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-21 09:37:18 +0100

    FIX: Added full hu-HU route in the hopes to fix production blog link issues

--- app/hu-HU/page.jsx
+++ app/hu-HU/page.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import BlogCard from "./components/BlogCard.jsx";
+import { about } from "./data/about.js";
 
-export default function HungarianPage() {
-  return <div>Hungarian Page</div>;
+export default function Home() {
+  return (
+    <div className=" mx-auto max-w-3xl ">
+      <div className=" mt-10 space-y-6">
+        <h1 className=" text-2xl">Hi I'm {about.nickName}</h1>
+        <p className=" text-lg">{about.bio}</p>
+      </div>
+
+      <section className=" mt-20">
+        <BlogCard />
+      </section>
+    </div>
+  );
 }

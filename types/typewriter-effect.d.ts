8f8fa2b4a4f9a2199e1337e27268544b3de0f353
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-25 14:33:36 +0100

    UDPATED: typewriter class with start & stop methods

--- types/typewriter-effect.d.ts
+++ types/typewriter-effect.d.ts
@@ -3,6 +3,8 @@ declare module "typewriter-effect/dist/core" {
     constructor(selector: string, options: any);
     typeString(str: string): this;
     pauseFor(ms: number): this;
+    start(): This;
+    stop(): This;
   }
 
   export = Typewriter;

331c87d0bd2c99559a8efcae2db07f857685010b
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-27 11:15:52 +0100

    INSTALLED: next-sanity ADDED: /lib/sanity.ts

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+import postEN from './postEN'
+import postHU from './postHU'
+import siteData from './siteData'
+
+export const schemaTypes = [siteData, postEN, postHU]

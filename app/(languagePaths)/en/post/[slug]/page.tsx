a8129a9cf7b2fc40640df6812f3fecdc9e198f4f
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-07-31 13:25:48 +0100

    UPDATE: generateMetadata in /en/post/[slug]

--- app/(languagePaths)/en/post/[slug]/page.tsx
+++ app/(languagePaths)/en/post/[slug]/page.tsx
@@ -11,6 +11,25 @@ async function getPost(slug: string) {
   return data;
 }
 
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  return {
+    title: (await getPost(params.slug)).title,
+    description: (await getPost(params.slug)).overview,
+  };
+}
+
+export async function generateStaticParams() {
+  const query = groq`*[_type == "postEN"]{slug}`;
+  const data = await client.fetch(query);
+  return data.map((post: { slug: string }) => {
+    return {
+      params: {
+        slug: post.slug,
+      },
+    };
+  });
+}
+
 export default async function page({ params }: { params: { slug: string } }) {
   const post = (await getPost(params.slug)) as Post;
 
